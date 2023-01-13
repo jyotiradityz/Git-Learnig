e89fd9c4957cfe30370a8ee1dd148788fc6841ac
jyotiradityz <patiljyotiraditya123@gmail.com>
2023-01-13 17:23:35 +0530

    ADDED CODE

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define jyot main
+#define lupin(a,b,c) for(int(a)=(b);(a)<(c);(a)++)
+#define lupim(a,b,c) for(int(a)=(b);(a)<=(c);(a)++)
+#define emp emplace_back
+#define Jyotiraditya ios_base::sync_with_stdio(false) ;
+#define Patil cin.tie(0);cout.tie(0) ;
+#define khtm cout<<endl ;
+#define int long long int
+using namespace std;
+int32_t jyot(){
+    Jyotiraditya Patil;
+    int t;cin>>t;
+        while(t--){
+            
+        }
+    return 0;
+}
